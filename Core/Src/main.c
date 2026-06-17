f32bfc90971b08413d31857acc8661a80815f1fc
Ekua2001 <baahcindy302@gmail.com>
2026-06-17 11:44:11 +0000

    Removed err

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -129,8 +129,7 @@ int main(void)
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
    // start freeRTOS app
-    int err = app_main();
-  UNUSED(err);
+
 
   Error_Handler(); // If app_main returns, there was an error. Handle it.
 
